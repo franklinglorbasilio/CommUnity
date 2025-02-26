3074509c6343555998bdd99cbcafeb15cd2462ea
franklinglorbasilio <franklinglorbasilio@gmail.com>
2025-02-26 13:46:04 +0800

    Updated project with new changes

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React, { useEffect } from "react";
 import Header from "./components/header";
 import Homepages from "./pages/frontpages/homepage";
 import PartnerBanner from "./pages/frontpages/PartnerBanner";
@@ -7,18 +8,43 @@ import CommunityProject from "./pages/frontpages/CommunityProject";
 import "bootstrap/dist/css/bootstrap.min.css";
 import CommUnity from "./pages/frontpages/CommUnity";
 import TestimonialPage from "./pages/frontpages/Testimonial";
+
 function App() {
+  useEffect(() => {
+    const handleContextMenu = (e) => {
+      e.preventDefault();
+    };
+
+    const handleKeyDown = (e) => {
+      if (
+          (e.ctrlKey && e.shiftKey && (e.key === "I" || e.key === "J")) ||
+          (e.ctrlKey && e.key === "U") ||
+          e.key === "F12"
+      ) {
+        e.preventDefault();
+      }
+    };
+
+    document.addEventListener("contextmenu", handleContextMenu);
+    document.addEventListener("keydown", handleKeyDown);
+
+    return () => {
+      document.removeEventListener("contextmenu", handleContextMenu);
+      document.removeEventListener("keydown", handleKeyDown);
+    };
+  }, []);
+
   return (
-   <>
-    <Header/>
-     <Homepages/>
-       <PartnerBanner/>
-       <CommUnity/>
-        <AboutCommunity/>
-       <CommunityPage/>
-       <CommunityProject/>
-       <TestimonialPage/>
-  </>
+      <>
+        <Header />
+        <Homepages />
+        <PartnerBanner />
+        <CommUnity />
+        <AboutCommunity />
+        <CommunityPage />
+        <CommunityProject />
+        <TestimonialPage />
+      </>
   );
 }
 
